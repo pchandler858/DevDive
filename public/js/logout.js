b68194f629fa7155dd1cf165173fc250848cf07c
Preston C <pchandler858@gmail.com>
2023-05-29 20:41:23 -0700

    logout alert

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,16 +1,16 @@
 // Initiate logout process
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-  
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    alert("You have been logged out.");
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector("#logout").addEventListener("click", logout);
